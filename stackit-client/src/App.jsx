4501b8eced9e6d91b25f90454e9b086fecb798e8
Sabari Doss R <sabursd18@gmail.com>
2025-07-12 15:32:52 +0530

    test : conflict resloved

--- stackit-client/src/App.jsx
+++ stackit-client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AskQuestion from "./pages/AskQuestion"; 
+import QuestionDetail from "./pages/QuestionDetail";
 
 import Navbar from "./components/Navbar";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/ask" element={<AskQuestion />} />
+        <Route path="/question/:id" element={<QuestionDetail />} />
       </Routes>
     </BrowserRouter>
   );
