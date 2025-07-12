e1f47f7c00bcfcd9ac9e5550dfdc05a8c9d8b7b8
kausikaaa <kauss0806@gmail.com>
2025-07-12 14:15:09 +0530

    feat(frontend) : update App (main)

--- stackit-client/src/App.jsx
+++ stackit-client/src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AskQuestion from "./pages/AskQuestion"; 
+
 import Navbar from "./components/Navbar";
-
-
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/ask" element={<AskQuestion />} />
       </Routes>
     </BrowserRouter>
   );
